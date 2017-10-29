dd1e635531949b43f03b001c2c2a483977e3a512
myJS.io Support <info@myJS.io>
2017-10-29 09:39:08 -0700

    0.1.0 Initial version

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
-# GetToDahChoppah
+# GetToDahChoppa
 An executable to generate multiple sub-jobs from indicated GCODE file, suitable for printing color-by-layer effects.
+
+## Overview
+This initial version works with the Cura slicer software. In subsequent versions, I may add support for other softwares but I would need access to GCODE examples first.
+
+There are other methods of printing 3D parts with different colors in vertical patterns. I have found these to often be problematic, leading to a hot extruder crashing into the part, for example. In another case, running the filament change wizard appeared *not* to reset the extruder positioning mode back to what it was before, resulting in the hot extruder drilling into my print bed.
+
+So I wanted to break up the original GCODE file into multiple ones. In theory, this should allow us to then send each job to the printer in order and to prep the printer in each case by running the appropriate filament change activity.
+
+If you're good with GCODE, you can then further edit each sub-job file to make sure that it's not doing some odd extruder assembly move which would take it through the path of the partially-printed part sitting on the print bed.
+
+Although untested at the time of this writing, you should be able to print different filament types per sub-job given that you can now edit each file for the temperature settings at the top.
+
+## Syntax
+This assumes that you've installed it per the instructions below so that it will be in your path.
+
+```
+# Example of the first file of two
+$ GetToDahChoppah -beginning -to 12 -ordinal 1 -count 2 -msg="First File" PathToGcodeFile
+Original:   /Users/user/Desktop/filename.gcode
+Slicer:     Cura_SteamEngine 2.3.1
+Layers:     24
+Slicing:
+  Output filename:  /Users/user/Desktop/filename_1of2.gcode
+  From:             0
+  To:               12
+  Msg:              "First File"
+  
+# Example of the second file of two
+$ GetToDahChoppah -from 13 -end -ordinal 2 -count 2 -msg="Second File" PathToGcodeFile
+Original:   /Users/user/Desktop/filename.gcode
+Slicer:     Cura_SteamEngine 2.3.1
+Layers:     24
+Slicing:
+  Output filename:  /Users/user/Desktop/filename_2of2.gcode
+  From:             13
+  To:               24
+  Msg:              "Second File"
+```
+
+## Installation
+The installation of this depends upon whether or not you also have a computer based upon the OS X operating system and further, whether or not you already have the Go compiler itself.
+
+### Mac
+Here are the instructions for installing this executable if you are on an Apple-based computer.
+
+#### If you have Go installed:
+
+```
+$ cd /usr/local/go/bin
+$ sudo curl https://github.com/OutsourcedGuru/GetToDahChoppa/raw/master/bin/GetToDahChoppa GetToDahChoppa
+$ cd ~/Desktop
+$ which GetToDahChoppa
+/usr/local/go/bin/GetToDahChoppa
+$ GetToDahChoppah -info PathToGcodeFile
+Original:   /Users/user/Desktop/filename.gcode
+Slicer:     Cura_SteamEngine 2.3.1
+Layers:     24
+
+Finished.
+```
+
+#### If you have don't have Go installed:
+
+```
+$ cd /usr/local/bin
+$ sudo curl https://github.com/OutsourcedGuru/GetToDahChoppa/raw/master/bin/GetToDahChoppa GetToDahChoppa
+$ cd ~/Desktop
+$ which GetToDahChoppa
+/usr/local/GetToDahChoppa
+$ GetToDahChoppah -help
+Usage of GetToDahChoppa:
+  -beginning
+    	start from first layer seen in file
+  -count int
+    	job count [-count=3] with "_1of3" appended to filename as output (default -1)
+  -end
+    	end at last layer seen in file
+  -from int
+    	start from indicated layer [-from=4] (default -1)
+  -info
+    	just read layer data from file
+  -msg string
+    	M117 message info to add to GCODE [-msg="Blue PLA"] (default "N/A")
+  -ordinal int
+    	job ordinal [-ordinal=1] (default -1)
+  -to int
+    	end at indicated layer [-to=24] (default -1)
+```
+
+### Windows or UNIX
+Here are the instructions for building this executable if you are on a different operating system.
+
+#### Install Go
+The first step is to [install the Go language compiler](https://golang.org).
+
+It's then usual to create a Go working folder under your user's profile.
+
+```
+# These two are optional, depending upon whether
+# or not you did this during the Go installation
+$ mkdir -p ~/go/src
+$ cd ~/go/src
+$ go get github.com/OutsourcedGuru/GetToDahChoppa/
+```
+
+This should download everything required and build it for you. Assuming that you installed Go correctly earlier and it's in your path, you should then be able to run it as in the instructions above.
+
+## Important Notes
+* The current version expects Cura as the slicer and has been tested with v2.3.1 of same. It has been tested with the Robo C2 printer with a single extruder and no heated bed.
+* Given the behavior of G29 autoleveling routines it is unwise to allow autoleveling on any of the sub-jobs because the nine-point testing should interfere with already-printed parts on the bed itself. For this reason I would strongly suggest toggling off autoleveling, per [OctoPrint-plugin-toggle-autolevel](https://github.com/OutsourcedGuru/OctoPrint-plugin-toggle-autolevel).
+* Additionally, it may be worth noting the current path(s) used during the printing of a priming line in the front of the printer so that the extruder assembly doesn't crash into your part. Proper placement of your part during slicing may be the best course of action here. Lastly, I would suggest that you remove each priming line as printed since each sub-job will attempt to recreate it.
+* You might consider installing [M117Popup](http://plugins.octoprint.org/plugins/M117PopUp/), [M117NavBar](http://plugins.octoprint.org/plugins/M117NavBar/) or [StatusLine](http://plugins.octoprint.org/plugins/status_line/) as well as [DisplayZ](http://plugins.octoprint.org/plugins/displayz/) while you're at it to take advantage of the `-msg` flag option of GetToDahChoppa.
---
+++ main.go
@@ -0,0 +1,211 @@
+/*
+GetToDahChoppa
+
+Description: An executable to generate multiple sub-jobs from indicated GCODE file,
+						 suitable for printing color-by-layer effects.
+
+Notes:       The current version expects Cura as the slicer and has been tested
+						 with v2.3.1 of same. It has been tested with the Robo C2 printer
+						 with a single extruder and no heated bed.
+
+						 Given the behavior of G29 autoleveling routines it is unwise to allow
+						 autoleveling on any of the sub-jobs because the nine-point testing
+						 should interfere with already-printed parts on the bed itself. For
+						 this reason I would strongly suggest toggling off autoleveling, per
+						 https://github.com/OutsourcedGuru/OctoPrint-plugin-toggle-autolevel.
+
+						 Additionally, it may be worth noting the current path(s) used
+						 during the printing of a priming line in the front of the printer
+						 so that the extruder assembly doesn't crash into your part. Proper
+						 placement of your part during slicing may be the best course of
+						 action here. Lastly, I would suggest that you remove each priming
+						 line as printed since each sub-job will attempt to recreate it.
+
+Author:      Michael Blankenship
+Repo:        https://github.com/OutsourcedGuru/GetToDahChoppa
+*/
+package main
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func syntax() {
+	fmt.Printf("Syntax: GetToDahChoppa [flags] GCodeFilePath\n\n")
+	fmt.Printf("        -beginning | -from=14   (selects starting layer to print)\n")
+	fmt.Printf("        -end | -to=24           (selects ending layer to print)\n")
+	fmt.Printf("        -ordinal=1 & -count=3   (will append \"_1of3\" for output filename)\n")
+	fmt.Printf("        -info                   (displays layer information in file only)\n")
+	fmt.Printf("        -msg=\"\"                 (adds M117 message information to GCODE file)\n")
+	fmt.Printf("\n")
+	os.Exit(1)
+}
+
+func main() {
+	bDoNotOverwriteEndLayerVariable := false
+	bReadError                      := false
+	bHeader                         := true
+	bFooter                         := false
+	currentlayer                    := -1
+	slicer                          := "N/A"
+	layers                          := "N/A"
+	inputfilename                   := "N/A"
+	outputfilename                  := "N/A"
+	dataOut, err                    := os.Create("/tmp/GetToDahChoppa")
+	beginning                       := flag.Bool("beginning", false, "start from first layer seen in file")
+	end                             := flag.Bool("end", false, "end at last layer seen in file")
+	from                            := flag.Int("from", -1, "start from indicated layer [-from=4]")
+	to                              := flag.Int("to", -1, "end at indicated layer [-to=24]")
+	ordinal                         := flag.Int("ordinal", -1, "job ordinal [-ordinal=1]")
+	count                           := flag.Int("count", -1, "job count [-count=3] with \"_1of3\" appended to filename as output")
+	info                            := flag.Bool("info", false, "just read layer data from file")
+	msg                             := flag.String("msg", "N/A", "M117 message info to add to GCODE [-msg=\"Blue PLA\"]")
+	beginlayer                      := -1
+	endlayer                        := -1
+	flag.Parse()
+	if len(flag.Args()) != 1                         { syntax() }		// Should be only one argument as a filename after flags
+	if (*beginning && *from != -1)                   { syntax() }		// Either -beginning or -from flag but not both
+	if (*end && *to != -1)                           { syntax() }		// Either -end or -to but not both
+	if (! *info && (*ordinal == -1 || *count == -1)) { syntax() }		// Either -info or both -ordinal and -count
+	
+	inputfilename = flag.Args()[0]
+	data, err := ioutil.ReadFile(inputfilename)
+	if err != nil {
+		bReadError = true;
+		fmt.Fprintf(os.Stderr, "GetToDahChoppa:\n  %v\n\n", err)
+		return
+	}
+
+	// Process the beginning wanted
+	if (! *beginning) {
+		// No -beginning flag seen
+		if (*from == -1) {
+			// No -from flag seen
+			beginlayer = 0
+		} else {
+			// -from flag seen
+			beginlayer = *from
+		}
+	} else {
+		// -beginning flag seen
+		beginlayer = 0
+	}
+
+	// Process the ending wanted
+	if (! *end) {
+		// No -end flag seen
+		if (*to == -1) {
+			// No -to flag seen
+			// We'll set this when we read the file, so leave it alone
+		} else {
+			// -to flag seen, so use that
+			endlayer = *to
+			bDoNotOverwriteEndLayerVariable = true
+		}
+	} else {
+		// -end flag seen
+		// We'll set this when we read the file, so leave it alone
+	}
+
+	// When finished, this should look like...                              /Users/user/Desktop/OriginalFilename_1of2.gcode
+	path, basefilename := filepath.Split(inputfilename)                    // OriginalFilename.gcode
+	basefilename = basefilename[0:strings.LastIndexAny(basefilename, ".")]	// OriginalFilename
+	outputfilename = fmt.Sprintf("%s%s_%dof%d%s", path, basefilename, *ordinal, *count, filepath.Ext(inputfilename))
+	// Attempt to create the output file
+	if (! *info) {
+		dataOut, err = os.Create(outputfilename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "GetToDahChoppa:\n  %v\n\n", err)
+			return
+		}
+	}
+
+	// Now process the input file into the output file
+	for _, line := range strings.Split(string(data), "\n") {
+		if strings.Contains(line, ";LAYER:")   {
+			currentlayer, err = strconv.Atoi(line[7:])
+		}
+		if bHeader {
+			if strings.Contains(line, ";Generated with") { slicer = line[16:] }
+			// This represents the last line of the Cura header
+			if strings.Contains(line, ";LAYER_COUNT:")   {
+				bHeader = false
+				layers = line[13:]
+				if (!bDoNotOverwriteEndLayerVariable) {
+					endlayer, err = strconv.Atoi(layers)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "\n  Error: %v\n\n", err)
+					}
+				}
+				if (*msg != "N/A") {
+					if (! *info) {
+						// ------------------------------------------------------------------------------
+						// Writing
+						// ------------------------------------------------------------------------------
+						dataOut.WriteString("M117 ")
+						dataOut.WriteString(*msg)
+						dataOut.WriteString("\n\n")
+					}
+				}
+			}
+				// We won't know the layer during the header so just write it to the output
+			if (! *info) {
+				// ------------------------------------------------------------------------------
+				// Writing
+				// ------------------------------------------------------------------------------
+				dataOut.WriteString(line + "\n")
+			}
+		} else {
+			if bFooter {
+				if (! *info) {
+					// ------------------------------------------------------------------------------
+					// Writing
+					// ------------------------------------------------------------------------------
+					dataOut.WriteString(line + "\n")
+				}
+			}
+			if strings.Contains(line, ";End of Gcode")   {
+				bFooter = true
+				if (! *info) {
+					// ------------------------------------------------------------------------------
+					// Writing
+					// ------------------------------------------------------------------------------
+					dataOut.WriteString("M107\n\nM104 S0\n")
+					dataOut.WriteString(line + "\n")
+				}
+			}
+			// Since we're out of the header, we now need to pay attention to the current layer, as known.
+			// Since we won't see another layer increment after the final layer, this should include
+			// any GCODE at the end of the file.
+			if currentlayer >= beginlayer && currentlayer <= endlayer {
+				if (! *info) {
+				// ------------------------------------------------------------------------------
+				// Writing
+				// ------------------------------------------------------------------------------
+				dataOut.WriteString(line + "\n")
+				}
+			}
+		}
+	}
+	dataOut.Sync()
+	dataOut.Close()
+	if !bReadError {
+		fmt.Printf("Original:   %s\n", inputfilename)
+		if (slicer != "N/A")        { fmt.Printf("Slicer:     %s\n", slicer) }
+		if (layers != "N/A")        { fmt.Printf("Layers:     %s\n", layers) }
+		// Abort early if we're just interested in information from the file
+		if (! *info) {
+			fmt.Printf("Slicing:\n")
+			fmt.Printf("  Output filename:  %s\n", outputfilename)
+			fmt.Printf("  From:             %d\n", beginlayer)
+			fmt.Printf("  To:               %d\n", endlayer)
+			if (*msg != "N/A") { fmt.Printf("  Msg:              %q\n", *msg) }
+		}
+		fmt.Printf("\nFinished.\n\n")
+	}
+}
